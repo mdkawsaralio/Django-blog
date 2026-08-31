8fa10defd67b8c71d9fad4cbcbb64de4f2316f9b
kawsar <kawsarnubtk05@gmail.com>
2026-08-31 09:39:06 +0600

    add 404 page and post by categories

--- blogs/models.py
+++ blogs/models.py
@@ -34,7 +34,8 @@ class Blog(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
-
+    def __str__(self):
+        return self.title
 
 
 
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+from blogs.models import Blog,Category
 # Create your views here.
+
+
+def post_by_category(request,category_id):
+    posts=Blog.objects.filter(status='Published',category=category_id)
+    try:
+        category=Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    # category=get_object_or_404(Category,pk=category_id)
+    context={
+        'posts':posts,
+        'category_id':category
+    }
+    return render(request,'post_by_category.html',context)
+    
